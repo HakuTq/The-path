9d8591b4d05079b4603f5b25655c5545551de531
HakuTq <hakutq05@gmail.com>
2025-03-28 22:58:07 +0100

    remade login and register css

--- resources/views/livewire/pages/auth/register.blade.php
+++ resources/views/livewire/pages/auth/register.blade.php
@@ -39,51 +39,51 @@ $register = function () {
 
 ?>
 
-<div>
-    <form wire:submit="register">
+<div class="whole-page-container">
+    <form wire:submit="register" class="register-form">
         <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input wire:model="name" id="name" class="block mt-1 w-full" type="text" name="name" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        <div class="register-form-field">
+            <x-input-label for="name" :value="__('Name')" class="register-form-label"/>
+            <x-text-input wire:model="name" id="name" class="register-form-input" type="text" name="name" required autofocus autocomplete="name" />
+            <x-input-error :messages="$errors->get('name')" class="register-form-label-error" />
         </div>
 
         <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input wire:model="email" id="email" class="block mt-1 w-full" type="email" name="email" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        <div class="register-form-field">
+            <x-input-label for="email" :value="__('Email')" class="register-form-label"/>
+            <x-text-input wire:model="email" id="email" class="register-form-input" type="email" name="email" required autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="register-form-label-error" />
         </div>
 
         <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+        <div class="register-form-field">
+            <x-input-label for="password" :value="__('Password')" class="register-form-label"/>
 
-            <x-text-input wire:model="password" id="password" class="block mt-1 w-full"
+            <x-text-input wire:model="password" id="password" class="register-form-input"
                             type="password"
                             name="password"
                             required autocomplete="new-password" />
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            <x-input-error :messages="$errors->get('password')" class="register-form-label-error" />
         </div>
 
         <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+        <div class="register-form-field">
+            <x-input-label for="password_confirmation" :value="__('Confirm Password')" class="register-form-label"/>
 
-            <x-text-input wire:model="password_confirmation" id="password_confirmation" class="block mt-1 w-full"
+            <x-text-input wire:model="password_confirmation" id="password_confirmation" class="register-form-input"
                             type="password"
                             name="password_confirmation" required autocomplete="new-password" />
 
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+            <x-input-error :messages="$errors->get('password_confirmation')" class="register-form-label-error" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}" wire:navigate>
+        <div class="register-form-bottom">
+            <a class="" href="{{ route('login') }}" wire:navigate>
                 {{ __('Already registered?') }}
             </a>
 
-            <x-primary-button class="ms-4">
+            <x-primary-button class="register-form-button">
                 {{ __('Register') }}
             </x-primary-button>
         </div>
--- resources/views/selection.blade.php
+++ resources/views/selection.blade.php
@@ -16,7 +16,7 @@
             <!--empty character (creates a new one) -->
             <!--paginate for every 3 chars (last character = new character)-->
             <a class="character-container-empty" href="{{ route('selection') }}">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" height="4rem" fill="white"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 144L48 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l144 0 0 144c0 17.7 14.3 32 32 32s32-14.3 32-32l0-144 144 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-144 0 0-144z"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" height="4rem" fill="white" stroke-width="8px" stroke="black"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 144L48 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l144 0 0 144c0 17.7 14.3 32 32 32s32-14.3 32-32l0-144 144 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-144 0 0-144z"/></svg>
                 <div class="character-name">New character</div>
             </a>
         </div>
