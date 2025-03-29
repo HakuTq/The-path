73287ac68fb2e03d96a640540ac291faed74a649
HakuTq <hakutq05@gmail.com>
2025-03-29 18:05:20 +0100

    remade profile css, remade routes a little

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,29 +1,31 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
+@extends('layouts.app')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
+@section('title', 'Profile')
+
+@section('content')
+    <div class="profile-page-container">
+        <div class="profile-header">
+            <h2>{{ __('Profile') }}</h2>
+        </div>
+
+        <div class="profile-content">
+            <div class="profile-section">
+                <div class="profile-section-content">
                     <livewire:profile.update-profile-information-form />
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
+            <div class="profile-section">
+                <div class="profile-section-content">
                     <livewire:profile.update-password-form />
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
+            <div class="profile-section">
+                <div class="profile-section-content">
                     <livewire:profile.delete-user-form />
                 </div>
             </div>
         </div>
     </div>
-</x-app-layout>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
+    
+    Route::view('dashboard', 'dashboard')->name('dashboard');
 
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
+    Route::view('profile', 'profile')->name('profile');
 
-Route::view('selection', 'selection')
-    ->name('selection');
+    Route::view('selection', 'selection')->name('selection');
+
+    });
 
 require __DIR__.'/auth.php';
