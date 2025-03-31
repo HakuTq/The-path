7693bb8a739ab6d23480f2c78865a69a1cd29877
EwDosbox <cada.vviktor@gmail.com>
2025-03-31 22:01:20 +0200

    Properties

--- app/Models/Character.php
+++ app/Models/Character.php
@@ -8,26 +8,42 @@ use Illuminate\Database\Eloquent\Model;
 
 class Character extends Model
 {
+    #region Traits
     use HasFactory;
-
-    protected $fillable = [
-        'user_id',
-        'name',
-        'gold',
-        'alignment',
-        'race',
-        'ability_scores',
-        'feats',
-        'skills',
-    ];
-
+    protected $fillable = ['user_id', 'name', 'gold', 'alignment', 'race', 'ability_scores', 'feats', 'skills',];
     protected $casts = [
         'ability_scores' => 'array',
         'feats' => 'array',
         'skills' => 'array',
         'alignment' => AlignmentEnum::class,
     ];
+    #endregion
+    #region Properties
+    public function getAbilityScore($ability):?int
+    {
+        return $this->ability_scores[$ability] ?? null;
+    }
+    public function getAbbilityModifier($ability):?int
+    {
+        $score = $this->getAbilityScore($ability);
+        if ($score === null) return null;
+        return floor(($score - 10) / 2);
+    }
 
+    public function getFeats():array
+    {
+        return $this->feats;
+    }
+    public function getSkills():array
+    {
+        return $this->skills;
+    }
+    public function getAlignment(): AlignmentEnum
+    {
+        return $this->alignment;
+    }
+    #endregion
+    #region Relationships
     public function user()
     {
         return $this->belongsTo(User::class);
@@ -36,4 +52,5 @@ class Character extends Model
     {
         return $this->hasMany(CharacterClass::class);
     }
+    #endregion
 }
